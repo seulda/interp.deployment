2db29114cfd6ecb7b40c3ac063d522551d93495c
seulda <amekimm@gmail.com>
2025-04-16 17:46:41 +0900

    Update interp.ia.api.sh
    git tag remote 설정 추가

--- scripts/interp.ia.api.sh
+++ scripts/interp.ia.api.sh
@@ -4,9 +4,13 @@ SOURCE_PATH=$1
 
 TAG_NAME=`date +%Y%m%d%H%M`
 
-git tag $TAG_NAME
-git push origin $TAG_NAME
-
 docker build --tag zeul/interp.ia.api:dev-latest --tag zeul/interp.ia.api:$TAG_NAME "$SOURCE_PATH"
 docker push zeul/interp.ia.api:dev-latest
 docker push zeul/interp.ia.api:$TAG_NAME
+
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+git config user.name "GitHub Actions"
+git config user.email "actions@github.com"
+
+git tag $TAG_NAME
+git push origin $TAG_NAME
