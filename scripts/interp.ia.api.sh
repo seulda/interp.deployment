a0416ac5ec21a59ad28df4e032a1ac32feef9bee
seulda <amekimm@gmail.com>
2025-04-16 15:31:36 +0900

    build: Update interp.ia.api.sh
    스크립트 파라미터 추가
    git 태그 주석 해제

--- scripts/interp.ia.api.sh
+++ scripts/interp.ia.api.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SOURCE_PATH=$1
+
 TAG_NAME=`date +%Y%m%d%H%M`
 
-#git tag $TAG_NAME
-#git push origin $TAG_NAME
+git tag $TAG_NAME
+git push origin $TAG_NAME
 
-docker build --tag zeul/interp.ia.api:dev-latest --tag zeul/interp.ia.api:$TAG_NAME .
+docker build --tag zeul/interp.ia.api:dev-latest --tag zeul/interp.ia.api:$TAG_NAME "$SOURCE_PATH"
 docker push zeul/interp.ia.api:dev-latest
 docker push zeul/interp.ia.api:$TAG_NAME
