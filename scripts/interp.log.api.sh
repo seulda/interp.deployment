ddd628e8b5804e06ff36504a620767685bc4f0c2
seulda <amekimm@gmail.com>
2025-04-18 13:08:13 +0900

    Build: 빌드 스크립트 내 git tag 제거

--- scripts/interp.log.api.sh
+++ scripts/interp.log.api.sh
@@ -2,9 +2,6 @@
 
 TAG_NAME=`date +%Y%m%d%H%M`
 
-#git tag $TAG_NAME
-#git push origin $TAG_NAME
-
 docker build --tag zeul/interp.log.api:dev-latest --tag zeul/interp.log.api:$TAG_NAME .
 docker push zeul/interp.log.api:dev-latest
 docker push zeul/interp.log.api:$TAG_NAME
